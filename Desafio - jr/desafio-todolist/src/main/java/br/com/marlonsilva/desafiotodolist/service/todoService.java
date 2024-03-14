e62ee7f0c9c81bd6e3845d2e8e84bb41f08fbd32
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-14 09:45:24 -0400

    create todoService.java

---
+++ Desafio - jr/desafio-todolist/src/main/java/br/com/marlonsilva/desafiotodolist/service/todoService.java
@@ -0,0 +1,42 @@
+package br.com.marlonsilva.desafiotodolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.marlonsilva.desafiotodolist.entity.todo;
+import br.com.marlonsilva.desafiotodolist.repository.todoRepository;
+
+@Service
+public class todoService {
+
+    private todoRepository TodoRepository;
+    
+    public todoService (todoRepository TodoRepository){
+        this.TodoRepository = TodoRepository;
+    }
+
+    public List<todo> create(todo Todo){
+        TodoRepository.save(Todo);
+        return list();
+    }
+
+    public List<todo> list(){
+        Sort sort = Sort.by("prioridades").descending().and(
+            Sort.by("nome").ascending()
+        );
+        return TodoRepository.findAll(sort);
+    }
+
+    public List<todo> update(todo Todo){
+        TodoRepository.save(Todo);
+        return list();
+    }
+
+    public List<todo> delete(Long id){
+        TodoRepository.deleteById(id);;
+        return list();
+    }
+    
+}
